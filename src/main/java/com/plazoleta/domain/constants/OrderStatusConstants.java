dae393c4b6a5e25440136281464677b00af4c840
Daniel Suarez <daniel.suarez@pragma.com.co>
2025-01-14 23:03:21 -0500

    feat: constantes

---
+++ src/main/java/com/plazoleta/domain/constants/OrderStatusConstants.java
@@ -0,0 +1,7 @@
+package com.plazoleta.domain.constants;
+
+public class OrderStatusConstants {
+    public static final String PENDING = "Pendiente";
+    public static final String IN_PREPARATION = "Preparacion";
+    public static final String READY = "Listo";
+}
